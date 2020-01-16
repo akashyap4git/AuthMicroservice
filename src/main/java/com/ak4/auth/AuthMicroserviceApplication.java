44181eb0f2edf7ce62b4c19e1877e8b56f3860fd
ambarishk <ambarishk@juniper.net>
2020-01-16 18:13:18 +0530

    Adding docker file and eureka discovery

--- src/main/java/com/ak4/auth/AuthMicroserviceApplication.java
+++ src/main/java/com/ak4/auth/AuthMicroserviceApplication.java
@@ -2,7 +2,9 @@ package com.ak4.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class AuthMicroserviceApplication {
 
